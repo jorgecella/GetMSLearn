ada51a23607c97874cc1994852d76ca455235927
Jorge Cella <jorgec@microsoft.com>
2020-09-17 18:14:09 -0300

    fix about the folders

--- GetMSLearnPath.py
+++ GetMSLearnPath.py
@@ -5,16 +5,16 @@ import os
 
 session = HTMLSession()
 
-webURL = 'https://docs.microsoft.com/es-mx/learn/modules/intro-to-azure-networking/'
+webURL = 'https://docs.microsoft.com/es-mx/learn/modules/intro-to-security-in-azure/2-shared-responsibility'
 origenURL = 'https://docs.microsoft.com'
 
+# path = os.getcwd()  +'\\'
 path = 'C:\\Users\\jorgec\\OneDrive\\Philanthropy Latam\\LearnDownloader\\Sites\\'
 r = session.get(webURL)
 r.html.render()
 
 image_files = []
 css_files = []
-
 filename= r.text
 
 
@@ -54,28 +54,28 @@ getlinks('img','src', image_files)
 for cssfiles in css_files:
     cssOK = (cssfiles.split('/')[-1].split('.')[-1])
     if 'css' in cssOK:
-        if not os.path.exists(path + '_themes'):
-            os.mkdir(path + '_themes')
-        if not os.path.exists(path + '_themes\\' + cssfiles.split('/')[-1]):
-            download('https://docs.microsoft.com' + cssfiles[0:cssfiles.rfind('/')] + '/' + cssfiles.split('/')[-1].split('.')[-2] + '.css'  , path + '_themes\\' + cssfiles.split('/')[-1].split('.')[-2] + '.css'  )
+        if not os.path.exists(pathSite + '_themes'):
+            os.mkdir(pathSite + '_themes')
+        if not os.path.exists(pathSite + '_themes\\' + cssfiles.split('/')[-1]):
+            download('https://docs.microsoft.com' + cssfiles[0:cssfiles.rfind('/')] + '/' + cssfiles.split('/')[-1].split('.')[-2] + '.css'  , pathSite + '_themes\\' + cssfiles.split('/')[-1].split('.')[-2] + '.css'  )
         # change links to css
         filename = filename.replace(cssfiles, '/Sites/_themes/' + cssfiles.split('/')[-1].split('.')[-2] + '.css' )
 
 # download images
 for linksImages in image_files:
-        if not os.path.exists(path+'media'):
-                os.mkdir(path+'media')
+        if not os.path.exists(pathSite+'media'):
+                os.mkdir(pathSite+'media')
         print ('Imagenes: ' + webURL[0:webURL.rfind('/')] + "/" + linksImages )
         if not os.path.exists(linksImages):
                 web = webURL[0:webURL.rfind('/')] + "/"
                 if '/' in linksImages[0]:
                     web = origenURL        
                 r = requests.get(web + linksImages)
-                with open(path + '\media\\' + linksImages.split('/')[-1].split('.')[-2] + '.' + linksImages.split('/')[-1].split('.')[-1]  , 'wb') as f:
+                with open(pathSite + '\media\\' + linksImages.split('/')[-1].split('.')[-2] + '.' + linksImages.split('/')[-1].split('.')[-1]  , 'wb') as f:
                         f.write(r.content)
                 filename = filename.replace(linksImages, 'media/' + linksImages.split('/')[-1].split('.')[-2] + '.' + linksImages.split('/')[-1].split('.')[-1] )
 
 # download webpage
-with io.open(path + "index.html", "w") as f:
+with io.open(pathSite + "index.html", "w") as f:
     f.write(filename)
 
